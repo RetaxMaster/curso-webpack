8eb321ffb62ca647fc7d481e6d425efbdcb86b2e
retaxmaster <retaxmaster@hotmail.com>
2021-04-08 21:41:35 -0500

    Adicion de optimizacion y hashes al proyecto

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
 
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
+        filename: "[name].[contenthash].js",
         assetModuleFilename: "assets/images/[hash][ext][query]"
     },
 
@@ -44,7 +46,7 @@ module.exports = {
                     options: {
                         limit: 10000,
                         mimeType: "application/font-woff",
-                        name: "[name].[ext]",
+                        name: "[name].[contenthash].[ext]",
                         outputPath: "./assets/fonts/",
                         publicPath: "./assets/fonts/",
                         esModule: false
@@ -62,7 +64,9 @@ module.exports = {
             filename: "./index.html"
         }),
 
-        new MiniCSSExtractPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: "assets/[name].[contenthash].css"
+        }),
 
         new CopyPlugin({
             patterns: [
@@ -73,6 +77,14 @@ module.exports = {
             ]
         }),
         
-    ]
+    ],
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
     
 }
